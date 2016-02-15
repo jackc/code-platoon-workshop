c262d81794e68a463b37d2ee030998d81196987e
Jack Christensen <jack@jackchristensen.com>
2016-02-15 09:39:29 -0600

    Solution to exercise 1 and notes

--- exercises/01-raindrops/raindrops_test.rb
+++ exercises/01-raindrops/raindrops_test.rb
@@ -26,5 +26,46 @@ class RaindropsTest < Minitest::Test
     assert_equal '1', Raindrops.convert(1)
   end
 
-  # Add tests here!
+  def test_combinations
+    [
+      [1, "1"],
+      [2, "2"],
+      [3, "Pling"],
+      [4, "4"],
+      [5, "Plang"],
+      [6, "Pling"],
+      [7, "Plong"],
+      [8, "8"],
+      [9, "Pling"],
+      [10, "Plang"],
+      [11, "11"],
+      [12, "Pling"],
+      [13, "13"],
+      [14, "Plong"],
+      [15, "PlingPlang"],
+      [16, "16"],
+      [17, "17"],
+      [18, "Pling"],
+      [19, "19"],
+      [20, "Plang"],
+      [21, "PlingPlong"],
+      [22, "22"],
+      [23, "23"],
+      [24, "Pling"],
+      [25, "Plang"],
+      [26, "26"],
+      [27, "Pling"],
+      [28, "Plong"],
+      [29, "29"],
+      [30, "PlingPlang"],
+      [31, "31"],
+      [32, "32"],
+      [33, "Pling"],
+      [34, "34"],
+      [35, "PlangPlong"],
+      [105, "PlingPlangPlong"]
+    ].each do |num, text|
+      assert_equal text, Raindrops.convert(num)
+    end
+  end
 end
